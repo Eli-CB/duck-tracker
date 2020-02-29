6e30fb0b97da2954f5cfb3c72f83c004094dcb1c
Eli Brooks <elibrks@gmail.com>
2020-02-29 10:01:30 -0800

    added form, added component and page directories

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import DateTimePicker from 'react-datetime-picker';
+
 import axios from 'axios';
 
 class App extends Component {
@@ -39,7 +44,45 @@ class App extends Component {
     // UI
     render() {
         const { data } = this.state;
-        return null;
+
+        return(
+
+            <div>
+                <link
+                    rel="stylesheet"
+                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+                    integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+                    crossOrigin="anonymous"
+                />
+                <NavBar/>
+                <Form>
+                    <Form.Group controlId="formDuckFeed">
+                        <Form.Label>Time of Feed</Form.Label>
+                        <Form.Control type="text" placeholder="Enter the time and day you fed the ducks"/>
+                    </Form.Group>
+                    <Form.Group controlId="formFoodFed">
+                        <Form.Label>Food Fed</Form.Label>
+                        <Form.Control type="text" placeholder="Enter the food you fed the ducks"/>
+                    </Form.Group>
+                    <Form.Group controlId="formLocationFed">
+                        <Form.Label>Location Fed</Form.Label>
+                        <Form.Control type="text" placeholder="Enter the location you fed the ducks"/>
+                    </Form.Group>
+                    <Form.Group controlId="formNumDucks">
+                        <Form.Label>Number of Ducks Fed</Form.Label>
+                        <Form.Control type="text" placeholder="Enter the number of ducks you fed"/>
+                    </Form.Group>
+                    <Form.Group controlId="formAmountFed">
+                        <Form.Label>Amount Fed to Ducks</Form.Label>
+                        <Form.Control type="text" placeholder="Enter the amount of food you fed the ducks"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+
+        )
     }
 }
 
